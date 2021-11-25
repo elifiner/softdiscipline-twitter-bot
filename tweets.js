fbb119935bf47a6b770e541fdd25443ac775ccaf
Eli Finer <eli.finer@gmail.com>
2021-11-25 01:11:04 +0000

    shorter questions

--- tweets.js
+++ tweets.js
@@ -7,7 +7,8 @@ module.exports = {
 
     1. What habit are you working on?
     2. How did it go today?
-    3. What can you improve for tomorrow?
+    3. Did you feel any resistance?
+    4. What can you improve for tomorrow?
 
     Reply below 👇
   `),
